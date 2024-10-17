4a2106ddbe66d319228fc70692ed40b24004eeeb
YOGEESH K P <yogeesh793@gmail.com>
2024-10-17 23:18:40 +0530

    Add files via upload

---
+++ Python_Demonstration.py
@@ -0,0 +1,70 @@
+# String 
+str = 'computer'
+print(str)
+str1="keyboard"
+print(str1)
+str2 = """Learn python in 
+easy way"""
+print(str2)
+print("Learn in python \n in easy way")
+print('mouse is \t to click') 
+print('Ravi\'s Dog')
+print("\"hi\"")
+
+# Size/Length of String
+print(len("computer"))
+print(len("compu\nter"))
+
+# Excercise
+x=5
+print(x)
+
+print(0b101)
+
+print(0B1011100)
+
+print(0o5)
+
+print(0o134)
+
+print(0x5)
+
+print(0X5C)
+
+print(0x270A)
+
+print(32.25)
+
+print(3.225e1)
+
+print(+25.003)
+
+print(-32.00003)
+
+print(+453.124e5)
+
+print(-472.02e3)
+
+print(25.03e-3)
+
+# Check Complex number
+
+print(2+3j)
+
+print(6j)
+
+# Bool
+
+print(True)
+
+print(False)
+
+# String Contant
+
+print("True")
+
+# Special Literal
+
+spl = None
+print(spl)
+
